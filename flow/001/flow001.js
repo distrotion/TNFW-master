91c8001e51a08c893ab67cb3b95c3c49cdad9eb0
Distrotion <sittichok.ho@gmail.com>
2021-09-08 11:08:54 +0700

    .

--- flow/001/flow001.js
+++ flow/001/flow001.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+x=0
+y =0
+
 router.get('/flow001', (req, res) => {
+
     res.send('Hello World from flow 001')
-  })
+})
+
+router.get('/test123', (req, res) => {
+   if(y==1){
+    res.send(`no`)
+   }else{
+y=1
+//------------------------
+
+
+
+//------------------------
+y=0
+   }
+    x++
+    res.send(`${x}`)
+})
 
 module.exports = router;
