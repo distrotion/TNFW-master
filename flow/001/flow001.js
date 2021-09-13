e0225f446df454d0ed0be51a6a224e3d5d13a299
Distrotion <sittichok.ho@gmail.com>
2021-09-13 18:41:03 +0700

    .

--- flow/001/flow001.js
+++ flow/001/flow001.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+var mongodb = require('./../../function/mongodb');
+
+
 x=0
-y =0
+y=0
 
 router.get('/flow001', (req, res) => {
 
@@ -13,16 +16,19 @@ router.get('/test123', (req, res) => {
    if(y==1){
     res.send(`no`)
    }else{
-y=1
-//------------------------
 
-
-
-//------------------------
-y=0
    }
     x++
     res.send(`${x}`)
+})
+
+router.get('/mongotest', async (req, res) => {
+  
+  // var output = await mongodb.insertMany("test","doc01",[{"data":2,"test":"haha"}]);
+  // var output = await mongodb.find("test","doc01",{"data":2});
+  var upd = await mongodb.update("test","doc01",{ "data":2 }, { $set: { b: 1 } });
+  var output = await mongodb.find("test","doc01",{"data":2});
+  res.json(output)
 })
 
 module.exports = router;
